1779b69a2cadf75a7dc85085d33d958b214c8c22
vamshidharreddy47 <132982718+vamshidharreddy47@users.noreply.github.com>
2023-05-10 12:53:27 +0530

    Add files via upload

---
+++ 6-LINUX-INT-QUEST.py
@@ -0,0 +1,104 @@
+## Linux online editor link to practice LINUX COMMANDS
+    https://www.tutorialspoint.com/linux_terminal_online.php
+
+# Do you have experience on Linux ?
+    1. Yes, I have experience on linux. I have worked on all linux basic commands like...grep, find, chmod, ps, kill
+
+# Which linux type you have used ?
+    2. I have worked on redhat version 8.5
+
+# How do you know the version in linux ?
+    1. cat /etc/*release or  cat /etc/redhat-release
+
+# Use of pwd commnad ?
+    To know the current working directory PATH
+        pwd
+
+# Use of ls command ?
+    ls is used to list all files and directories
+    ## https://www.geeksforgeeks.org/practical-applications-ls-command-linux/
+
+# Use of ll or ls -l command ?
+    shows file or directory, size, modified date and time, file or folder name and owner of file and its permission.
+        ll
+        ls -l
+
+# Use of cp command ?
+    cp stands for copy. This command is used to copy files and directory
+    ## https://www.geeksforgeeks.org/cp-command-linux-examples/
+        cp RRR/today.py SSS/today.py
+        cp -r RRR SSS  #     recursive copying of directories.
+
+# Use of mv command ?
+    mv stands for move. mv is used to move files or directories from one path to another path
+    ## https://www.geeksforgeeks.org/mv-command-linux-examples/
+        mv RRR/today.py SSS/today.py
+        mv RRR SSS
+
+# Use of rm command ?
+    rm is used to remove files and directories.
+        rm today.py
+
+# How do you create a directory in Linux ?
+    Using mkdir command
+        mkdir RRR
+
+# How do you remove a directory in Linux ?
+    Using rmdir command
+        rmdir RRR
+
+# How do you create a empty file in Linux ?
+    Using touch command
+        touch today.py
+
+# Use cd(change directory) command 
+    cd command is used to change current working directory.
+        cd SSS
+
+
+# Use of grep command ?
+    1. The grep command which stands for “global regular expression print,” processes text line by line and prints any lines which match a specified pattern.
+    2. The grep command is used to search text or searches the given file for lines containing a match to the given strings.
+    ## Example # https://www.geeksforgeeks.org/grep-command-in-unixlinux/
+    * grep sriram today.txt    # To print which lines contains sriram word in today.txt
+    * grep -o sriram today.txt # To print only sriram word how many times occured in a today.txt
+
+# Use of find command ?
+    1. It can be used to find files and directories.
+    ## Example # https://www.tecmint.com/35-practical-examples-of-linux-find-command/
+
+# Use of chmod command ?
+    The chmod command is used to change the permissions of files or directories..
+    4 stands for "read",
+    2 stands for "write",
+    1 stands for "execute", and
+    # To give all permissions to today.txt file
+    chmod 777 today.txt
+
+        https://www.geeksforgeeks.org/chmod-command-linux/
+
+# Use of ps command ?
+    1. Linux provides us a utility called ps for viewing information related with the processes on a system which stands as abbreviation for “Process Status”. ps command is used to list the currently running processes and their PIDs along with some other information depends on different options
+    ## https://www.geeksforgeeks.org/ps-command-in-linux-with-examples/
+
+
+# How do you kill process in linux ?
+    1. Using kill command 
+    # example : kill -9 9876(Proceess id )
+
+# Use of cat command ?
+    1. Cat(concatenate) command is very frequently used in Linux. It reads data from the file and gives their content as output. It helps us to create, view, concatenate files.
+    cat filename.py          ==> To read file 
+    cat > filename.py        ==> To create a file
+    cat >> filename.py       ==> To Append to existing file
+    ## https://www.geeksforgeeks.org/cat-command-in-linux-with-examples/
+
+# What is vi editor ?
+    1. The default editor that comes with the UNIX operating system is called vi (visual editor). Using vi editor, we can edit an existing file or create a new file from scratch. we can also use this editor to just read a text file.
+    ## https://www.geeksforgeeks.org/vi-editor-unix/
+
+# How to konw IP address in linux ?
+    1. Using ifconfig
+
+
+
